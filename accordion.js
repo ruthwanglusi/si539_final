f2e6ac5790964f6a420b96043980824848576bcd
Lusi Wang <ruthwanglusi@gmail.com>
2019-04-18 22:39:05 -0400

    15th commit

--- accordion.js
+++ accordion.js
@@ -9,10 +9,10 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
+    if (panel.style.display === "grid") {
       panel.style.display = "none";
     } else {
-      panel.style.display = "block";
+      panel.style.display = "grid";
     }
   });
 }
